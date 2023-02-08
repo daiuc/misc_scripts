fb9a3172208e23e269d3de4e262c9ea742949659
daiuc <chaodai@uchicago.edu>
2023-02-08 11:50:39 -0600

    bug fixing and README edits

--- midway/make_rstudio_sbatch_shell/test.sh
+++ midway/make_rstudio_sbatch_shell/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
-#SBATCH --time {{ TIME }}
-#SBATCH -p {{ PARTITION }}
-#SBATCH -c {{ CPU }}
-#SBATCH --mem {{ MEM }}
-#SBATCH --job-name={{ JOBNAME}}
-#SBATCH --account={{ ACCT }}
-#SBATCH --output={{ LOGFILE }}
+#SBATCH --time 35:59:00
+#SBATCH -p broadwl
+#SBATCH -c 2
+#SBATCH --mem 20g
+#SBATCH --job-name=rstudio
+#SBATCH --account=pi-yangili1
+#SBATCH --output=logs/sbatchLogRstudioContainer.log
 
 
 
@@ -14,19 +14,19 @@
 ##-----------------------------------------------------##
 
 
-log={{ LOGFILE }}
+log=logs/sbatchLogRstudioContainer.log
 
 cd ~ && source ~/.bash_profile && pwd >$log
 
 echo -e "\n Submited job: $SLURM_JOB_ID\n\n\n" >> $log
 
-module load {{ SINGULARITY_VER }} &>> $log
-conda activate {{ CONDAENV }} &>> $log
+module load singularity/3.4.0 &>> $log
+conda activate smk &>> $log
 
 
-JPORT={{ JPORT }} # configured in .jupyter/jupyter_server_config.py
-RPORT={{ RPORT }} # for rstudio
-CPORT={{ CPORT }} # configured in .config/code-server/config.yaml
+JPORT=9798 # configured in .jupyter/jupyter_server_config.py
+RPORT=8282 # for rstudio
+CPORT= # configured in .config/code-server/config.yaml
 
 IP=$(/sbin/ip route get 8.8.8.8 | awk '{print $NF;exit}')
 echo -e "### DATE: $(date) ### \n" >> $log
@@ -60,29 +60,29 @@ jupyter lab --port=$JPORT  >> $log &
 echo -e "\n\n### 2.  Rstudio ###"
 echo "rstudio server running on http://${IP}:${RPORT}" >> $log
 ## set SIF
-SIF={{ SIF }}
+SIF=/scratch/midway2/chaodai/singularity/bajiame_rstudio_rstudio_2022_12.sif
 #SIF="/scratch/midway2/chaodai/singularity/rstudio_R4.1.0-Rstudio2022.12-v2.sif"
-RSTUDIO_TMP={{ RSTUDIO_TMP }}
+RSTUDIO_TMP=/scratch/midway2/chaodai/singularity/rstudio-tmp
 
 echo "using image $SIF" >> showRstudioAddress.txt
 echo -e "---------------\n\n\n"
 # set conda, R, python binary
-CONDA_PREFIX={{ CONDA_PREFIX }}
+CONDA_PREFIX=/scratch/midway2/chaodai/miniconda3/envs/smk
 R_BIN=$CONDA_PREFIX/bin/R
 PY_BIN=$CONDA_PREFIX/bin/python
 
 ## export conda environment to container
-export SINGULARITYENV_USER={{ SINGULARITYENV_USER }}
+export SINGULARITYENV_USER=chaodai
 export SINGULARITYENV_RSTUDIO_WHICH_R=${R_BIN}
 export SINGULARITYENV_CONDA_PREFIX=${CONDA_PREFIX}
-export SINGULARITYENV_PATH={{ SINGULARITYENV_PATH }}
-export SINGULARITYENV_CACHEDIR={{ SINGULARITYENV_CACHEDIR }}
-export SINGULARITYENV_MODULES_CMD={{ SINGULARITYENV_MODULES_CMD }}
-export SINGULARITYENV_MODULEPATH={{ SINGULARITYENV_MODULEPATH }}
+export SINGULARITYENV_PATH=/opt/pyenv/plugins/pyenv-virtualenv/shims:/home/chaodai/.pyenv/shims:/opt/pyenv/bin:/home/chaodai/.local/bin:/usr/lib/rstudio-server/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/chaodai/bin:/home/chaodai/.local/bin:/scratch/midway2/chaodai/miniconda3/envs/smk/bin
+export SINGULARITYENV_CACHEDIR=/scratch/midway2/chaodai/singularity/singularity_cache
+export SINGULARITYENV_MODULES_CMD=/software/modules/libexec/modulecmd.tcl
+export SINGULARITYENV_MODULEPATH=/software/modules/modulefiles:/software/modulefiles2
 
 
-RSTUDIO_SERVER_USER={{ RSTUDIO_SERVER_USER }} # change to your own
-COOKIE_KEY={{ COOKIE_KEY }}
+RSTUDIO_SERVER_USER=chaodai # change to your own
+COOKIE_KEY=/scratch/midway2/chaodai/singularity/rstudio-tmp/secure-cookie-key
 
 ## make sure these directories and files exists
 mkdir -p $RSTUDIO_TMP/var/lib $RSTUDIO_TMP/var/run $RSTUDIO_TMP/tmp
@@ -99,13 +99,13 @@ fi
 sleep 5 
 
 ### you can alternatively save your password to env variable and replace explicit password with the env variable
-PASSWORD='{{ RSTUDIO_PASSWORD }}' singularity exec \
+PASSWORD='password' singularity exec \
     --bind $RSTUDIO_TMP/var/lib:/var/lib/rstudio-server \
     --bind $RSTUDIO_TMP/var/run:/var/run/rstudio-server \
     --bind $RSTUDIO_TMP/tmp:/tmp \
     --bind $RSTUDIO_TMP/database.conf:/etc/rstudio/database.conf \
     --bind /sys/fs/cgroup/:/sys/fs/cgroup/:ro \
-    {{ CUSTOM_BIND }} \
+     --bind /home/chaodai:/home/rstudio --bind /project2/yangili1:/project2/yangili1 --bind /project2/jstaley:/project2/jstaley --bind /scratch/midway2/chaodai:/scratch/midway2/chaodai --bind /software:/software  \
     $SIF \
     rserver --server-user $RSTUDIO_SERVER_USER \
         --rsession-which-r=${R_BIN} \
@@ -115,7 +115,6 @@ PASSWORD='{{ RSTUDIO_PASSWORD }}' singularity exec \
         --auth-timeout-minutes=0 \
         --auth-stay-signed-in-days=30 \
         --secure-cookie-key-file=${COOKIE_KEY}
-
 
 
 
