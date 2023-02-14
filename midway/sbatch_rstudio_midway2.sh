da38a995dd03aaa82f5b0b11f43ffe6b667da9f4
daiuc <chaodai@uchicago.edu>
2023-02-14 11:06:53 -0600

    minor tricks

--- midway/sbatch_rstudio_midway2.sh
+++ midway/sbatch_rstudio_midway2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 #SBATCH --time 35:59:00
 #SBATCH -p broadwl
-#SBATCH -c 4
+#SBATCH -c 6
 #SBATCH --mem 30g
 #SBATCH --job-name=rstudio
 #SBATCH --account=pi-yangili1
@@ -15,14 +15,13 @@
 ##-----------------------------------------------------##
 
 
-log=logs/sbatchLogRstudioContainer.log
 
-cd ~ && source ~/.bash_profile && pwd >$log
+cd ~ && source ~/.bash_profile && pwd 
 
-echo -e "\n Submited job: $SLURM_JOB_ID\n\n\n" >> $log
+echo -e "\n Submited job: $SLURM_JOB_ID\n\n\n" 
 
-module load singularity/3.4.0 &>> $log
-conda activate smk &>> $log
+module load singularity/3.4.0 gcc/10.1.0
+conda activate smk
 
 
 JPORT=9798 # configured in .jupyter/jupyter_server_config.py
@@ -30,8 +29,8 @@ RPORT=8282 # for rstudio
 CPORT=8783 # configured in .config/code-server/config.yaml
 
 IP=$(/sbin/ip route get 8.8.8.8 | awk '{print $NF;exit}')
-echo -e "### DATE: $(date) ### \n" >> $log
-echo -e "### IP: ${IP}\n\n" >> $log
+echo -e "### DATE: $(date) ### \n" 
+echo -e "### IP: ${IP}\n\n" 
 
 
 ##-----------------------------------------------------##
@@ -39,10 +38,9 @@ echo -e "### IP: ${IP}\n\n" >> $log
 ##-----------------------------------------------------##
 
 echo -e "\n\n### 1.  Jupyter ###"
-echo -e "jupyter server runing on http://${IP}:${JPORT}/lab \n" >> $log
+echo -e "jupyter server runing on http://${IP}:${JPORT}/lab \n" 
 
-#jupyter lab --ip=${IP}  >> showJupyterAddress.txt &
-jupyter lab --port=$JPORT  >> $log &
+jupyter lab --port=$JPORT  &
 
 
 ##-----------------------------------------------------##
@@ -59,10 +57,11 @@ jupyter lab --port=$JPORT  >> $log &
 ##             Launch rstudio server                   ##
 ##-----------------------------------------------------##
 echo -e "\n\n### 2.  Rstudio ###"
-echo "rstudio server running on http://${IP}:${RPORT}" >> $log
+echo "rstudio server running on http://${IP}:${RPORT}" 
+
 ## set SIF
+#SIF="/scratch/midway2/chaodai/singularity/rstudio_r4.2.2-rstudio2022.12.0-v1.sif"
 SIF="/scratch/midway2/chaodai/singularity/bajiame_rstudio_rstudio_2022_12.sif"
-#SIF="/scratch/midway2/chaodai/singularity/rstudio_R4.1.0-Rstudio2022.12-v2.sif"
 RSTUDIO_TMP=/scratch/midway2/chaodai//singularity/rstudio-tmp
 
 echo "using image $SIF" >> showRstudioAddress.txt
@@ -76,6 +75,7 @@ PY_BIN=$CONDA_PREFIX/bin/python
 export SINGULARITYENV_USER=chaodai
 export SINGULARITYENV_RSTUDIO_WHICH_R=${R_BIN}
 export SINGULARITYENV_CONDA_PREFIX=${CONDA_PREFIX}
+#export SINGULARITYENV_PATH="/scratch/midway2/chaodai/miniconda3/envs/smk/bin:/scratch/midway2/chaodai/miniconda3/condabin:/software/gsl-2.5-el7-x86_64/bin:/software/tmux-3.1c-el7-x86_64/bin:/software/libevent-2.1.8-el7-x86_64/bin:/bin:/home/chaodai/bin:/home/chaodai/.local/bin:/software/slurm-current-el7-x86_64/bin:/software/git-2.10-el7-x86_64/bin:/software/subversion-1.9.4-el7-x86_64/bin:/software/bin:/srv/adm/bin:/software/modules/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:/opt/ibutils/bin:/usr/lpp/mmfs/bin:/software/systools/bin:\$PATH"
 export SINGULARITYENV_PATH="/opt/pyenv/plugins/pyenv-virtualenv/shims:/home/chaodai/.pyenv/shims:/opt/pyenv/bin:/home/chaodai/.local/bin:/usr/lib/rstudio-server/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/home/chaodai/bin:/home/chaodai/.local/bin:/scratch/midway2/chaodai/miniconda3/envs/smk/bin:\$PATH"
 export SINGULARITYENV_CACHEDIR="/scratch/midway2/chaodai/singularity/singularity_cache"
 export SINGULARITYENV_RSTUDIO_PASS=$RSTUDIO_PASS
@@ -86,19 +86,21 @@ export SINGULARITYENV_MODULEPATH=/software/modules/modulefiles:/software/modulef
 RSTUDIO_SERVER_USER=chaodai # change to your own
 
 ## run container app
-sleep 5 
+sleep 5
+
 # make sure MY_PASS is exported in your bash profile
-PASSWORD=${MY_PASS} singularity exec \
+PASSWORD=${RSTUDIO_PASS} singularity exec \
     --bind $RSTUDIO_TMP/var/lib:/var/lib/rstudio-server \
     --bind $RSTUDIO_TMP/var/run:/var/run/rstudio-server \
     --bind $RSTUDIO_TMP/tmp:/tmp \
     --bind $RSTUDIO_TMP/database.conf:/etc/rstudio/database.conf \
+    --bind $RSTUDIO_TMP/rsession.conf:/etc/rstudio/rsession.conf \
+    --bind $RSTUDIO_TMP/rserver.conf:/etc/rstudio/rserver.conf \
+    --bind $RSTUDIO_TMP/logging.conf:/etc/rstudio/logging.conf \
+    --bind $RSTUDIO_TMP/file-locks:/etc/rstudio/file-locks \
     --bind /sys/fs/cgroup/:/sys/fs/cgroup/:ro \
     --bind /home/chaodai:/home/rstudio \
-    --bind /project2/yangili1:/project2/yangili1 \
-    --bind /project2/jstaley:/project2/jstaley \
-    --bind /scratch/midway2/chaodai:/scratch/midway2/chaodai \
-    --bind /software:/software \
+    --bind /project2,/scratch/midway2/chaodai,/software \
     $SIF \
     rserver --server-user $RSTUDIO_SERVER_USER \
         --rsession-which-r=${R_BIN} \
@@ -109,16 +111,6 @@ PASSWORD=${MY_PASS} singularity exec \
         --auth-stay-signed-in-days=30 \
         --secure-cookie-key-file=/home/chaodai/rstudio-server/secure-cookie-key
 
-
-# CHANGES TO SINGULARITY COMMAND PARAMETERS:
-# 8/1/22: added option --server-user chaodai. `rstudio-server` from the 
-#   updated docker image requires this option.
-#
-# 6/1/22: The following binding directories are removed. Instead, now bind
-#   one level up:
-#       --bind /project2/xuanyao/chao:/mnt/ds2
-#       --bind /project2/yangili1/cdai:/mnt/ds1 \
-#       --bind /scratch/midway2/chaodai:/mnt/ds3 \
 
 
 
