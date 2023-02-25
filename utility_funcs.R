88acd92d09d901fd4869aaaad23e533192adb914
daiuc <chaodai@uchicago.edu>
2023-02-25 17:17:20 -0600

    added utility R funcs

---
+++ utility_funcs.R
@@ -0,0 +1,64 @@
+#' Frequently re-used custom functions
+
+
+readNominal = function(nominal.f, nominal.suf = "Nominal.txt.gz", gr, nom_cols) {
+  #' @description  : read nominal pass results into a data.table
+  #'
+  #' @nominal.f    : character, tabix indexed nominal pass result file prefix
+  #' @nominal.suf  : character, suffice for the nominal file, if nominal.f is
+  #'                 given as prefix, default "Nominal.txt.gz"
+  #' @gr           : GRange object, of the queried region
+  #' @pid_col      : character, column name for phenotype id
+  #' @nom_cols     : vector, column nmaes for nominal pass file
+  #'
+  #' @return       : data.table, nominal pass results for the queried region
+  #'                 for the phenotype
+
+  c = seqnames(gr) %>% as.vector
+  s = start(gr)
+  e = end(gr)
+
+  if (file.info(nominal.f)$isdir) {
+    # fill in full file path using chrom from gr, and prefix, suffix
+    f = glue(nominal.f, "/", {c}, "/", {nominal.suf})
+  } else {
+    f = nominal.f
+  }
+  if (!file.exists(f)) stop(glue("File {f} does not exist!"))
+
+  CMD = glue("tabix {f} {c}:{s}-{e}")
+  dt = fread(cmd = CMD, col.names = nom_cols) # read nominal snps in  region
+  if (nrow(dt) > 0) {
+    dt = dt[pid %in% mcols(gr)[['pid']]] # only want snps for given pheno
+  }
+
+  return(dt)
+}
+
+
+concatLabels <- function(data, byCol, oldLabel, newLabel) {
+        #' @param data    : data.table
+        #' @param byCol   : group by column, typically key
+        #' @param oldLabel: label to concat
+        #' @param newLabel: label after concat
+    
+        env = list(byCol = byCol, oldLabel = oldLabel, newLabel = newLabel)
+        env = lapply(env, as.name)
+        expr1 = substitute(paste0(unique(oldLabel), collapse = ","), env)
+        expr2 = substitute(byCol, env)
+        
+        data = eval(substitute(data[, .(new = expr1), by = expr2]))
+        setnames(data, "new", newLabel)
+        unique(data)
+}
+
+
+
+
+
+
+
+
+
+
+
