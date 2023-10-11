6d3534e03a2915a8ebc82655ed86dd194e014955
daiuc <chaodai@uchicago.edu>
2023-10-11 15:54:14 -0500

    new sif using customed rocker/rstudio:4.3.1

--- midway/sbatch_rstudio_midway2.sh
+++ midway/sbatch_rstudio_midway2.sh
@@ -60,8 +60,8 @@ echo -e "\n\n### 2.  Rstudio ###"
 echo "rstudio server running on http://${IP}:${RPORT}" 
 
 ## set SIF
-#SIF="/scratch/midway2/chaodai/singularity/bajiame_rstudio_rstudio_2022_12.sif"
-SIF="/scratch/midway2/chaodai/singularity/rstudio-2023_06.sif"
+# SIF="/scratch/midway2/chaodai/singularity/rstudio-2023_06.sif"
+SIF="/scratch/midway2/chaodai/singularity/rstudio-2023_10.sif"
 RSTUDIO_TMP=/scratch/midway2/chaodai//singularity/rstudio-tmp
 
 echo "using image $SIF" >> showRstudioAddress.txt
